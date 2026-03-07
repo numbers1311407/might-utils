c8f75cfb8547d46b08d8fef8a4d595b378bbcb58
travis <numbers1311407@gmail.com>
2026-03-07 11:54:30 -0500

    adjust vite base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/might-utils/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
