a7c874aca56d4770c5d7d683cba4e3b1c4bb18f5
travis <numbers1311407@gmail.com>
2026-03-14 13:08:55 -0400

    use might consts for min/max char level

--- src/features/roster/schema/char.js
+++ src/features/roster/schema/char.js
@@ -1,14 +1,17 @@
 import * as z from "zod";
 import { capitalize } from "@/utils";
 import { charClassSchema } from "./char-class";
+import { MightMinLevel, MightMaxLevel } from "@/common/might";
+
+const invalidLevelMessage = `Must be a might-enabled level ${MightMinLevel}-${MightMaxLevel}`
 
 export const charSchema = z.object({
   active: z.boolean().default(true),
   class: charClassSchema,
   level: z.coerce
-    .number({ message: "Must be a valid level 1-71" })
-    .min(1, { message: "Must be a valid level 1-71" })
-    .max(71, { message: "Must be a valid level 1-71" }),
+    .number({ message: invalidLevelMessage })
+    .min(MightMinLevel, { message: invalidLevelMessage })
+    .max(MightMaxLevel, { message: invalidLevelMessage }),
   name: z
     .string()
     .min(1, {
