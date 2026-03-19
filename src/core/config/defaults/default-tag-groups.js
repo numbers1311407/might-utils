293aa393999093a5a93d6b430ab25ca701bf36e5
travis <numbers1311407@gmail.com>
2026-03-19 16:11:58 -0400

    default ids for default tag groups to preserve option

--- src/core/config/defaults/default-tag-groups.js
+++ src/core/config/defaults/default-tag-groups.js
@@ -1,10 +1,12 @@
 export const defaultTagGroups = [
   {
     name: "Roles",
+    id: "defaults-roles",
     tags: ["dps", "healer", "support", "tank"],
   },
   {
     name: "Roles - Split DPS",
+    id: "defaults-roles-split",
     tags: ["mdps", "rdps", "healer", "support", "tank"],
   },
 ];
--- src/core/schemas/tag-group.js
+++ src/core/schemas/tag-group.js
@@ -3,7 +3,11 @@ import * as z from "zod";
 import { tagSchema } from "./tag.js";
 
 export const tagGroupSchema = z.object({
-  id: z.nanoid().default(() => nanoid()),
+  id: z.union([
+    z.nanoid().default(() => nanoid()),
+    z.literal("defaults-roles"),
+    z.literal("defaults-roles-split"),
+  ]),
   name: z.string(),
   tags: z.array(tagSchema).default([]),
   active: z.boolean().default(true),
