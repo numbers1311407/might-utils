2d9a07e25a4f38d590099c3f55c1cd7c9db22602
travis <numbers1311407@gmail.com>
2026-03-19 11:22:22 -0400

    add split dps tag group

--- src/core/config/defaults/default-tag-groups.js
+++ src/core/config/defaults/default-tag-groups.js
@@ -3,4 +3,8 @@ export const defaultTagGroups = [
     name: "Roles",
     tags: ["dps", "healer", "support", "tank"],
   },
+  {
+    name: "Roles - Split DPS",
+    tags: ["mdps", "rdps", "healer", "support", "tank"],
+  },
 ];
