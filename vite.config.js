1763b431c977a291d852feba0ec27bcb1ac11886
travis <numbers1311407@gmail.com>
2026-03-19 10:35:15 -0400

    add react compiler & linter

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
   base: "/might-utils/",
   resolve: {
     alias: {
