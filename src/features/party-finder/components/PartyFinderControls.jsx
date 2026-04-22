9bf9f5ad1a693b2e972bf9b3f29ad28936937200
travis <numbers1311407@gmail.com>
2026-04-22 12:15:48 -0400

    party size min/max uses range slider

--- src/features/party-finder/components/PartyFinderControls.jsx
+++ src/features/party-finder/components/PartyFinderControls.jsx
@@ -1,22 +1,31 @@
-import { Group, Paper, Stack } from "@mantine/core";
+import { useCallback, useMemo } from "react";
+import { Group, Paper, Stack, InputWrapper, RangeSlider } from "@mantine/core";
 import { useThrottledCallback } from "@mantine/hooks";
+import { useDraftState, useStableCallback } from "@/core/hooks";
 import { ActiveTagFiltersSelect, NumberField } from "@/core/components";
 import { MightMaxLevel, MightMinLevel } from "@/config/might";
+import { useTagRulesStore } from "@/model/store";
 import { usePartyFinderStore } from "../store";
 import { ResultsGroupingSelect } from "./ResultsGroupingSelect.jsx";
 
-export const PartyOptionInput = ({ option, ...restProps }) => {
+const usePartyOption = (option) => {
   const setOption = usePartyFinderStore((store) => store.setOption);
   const options = usePartyFinderStore((store) => store.options);
   const value = options[option];
   const setValue = useThrottledCallback((value) => {
     setOption(option, value === "" ? undefined : value);
   }, 400);
+
+  return [value, setValue];
+};
+
+export const PartyOptionNumberField = ({ option, ...restProps }) => {
+  const [value, setValue] = usePartyOption(option);
   return <NumberField value={value} setValue={setValue} {...restProps} />;
 };
 
 const MaxLevelInput = (props) => (
-  <PartyOptionInput
+  <PartyOptionNumberField
     option="maxLevel"
     label="Max Char Level"
     placeholder="The carries"
@@ -27,7 +36,7 @@ const MaxLevelInput = (props) => (
 );
 
 const MinLevelInput = (props) => (
-  <PartyOptionInput
+  <PartyOptionNumberField
     option="minLevel"
     label="Min Char Level"
     placeholder="The twinks"
@@ -37,30 +46,8 @@ const MinLevelInput = (props) => (
   />
 );
 
-const MaxSizeInput = (props) => (
-  <PartyOptionInput
-    option="maxSize"
-    label="Max Party Size"
-    placeholder="Big party"
-    min={1}
-    max={20}
-    {...props}
-  />
-);
-
-const MinSizeInput = (props) => (
-  <PartyOptionInput
-    option="minSize"
-    label="Min Party Size"
-    placeholder="Lil' party"
-    min={1}
-    max={20}
-    {...props}
-  />
-);
-
 const MarginInput = (props) => (
-  <PartyOptionInput
+  <PartyOptionNumberField
     option="margin"
     label="Score Tolerance"
     help="Acceptable deviation under your target might score. Must be higher than the lowest might score on your roster."
@@ -72,7 +59,7 @@ const MarginInput = (props) => (
 );
 
 const TargetScoreInput = (props) => (
-  <PartyOptionInput
+  <PartyOptionNumberField
     option="targetScore"
     label="Target Might Score"
     description="What might score are you trying to hit?"
@@ -84,6 +71,68 @@ const TargetScoreInput = (props) => (
   />
 );
 
+const SizeRangeInput = (props) => {
+  const {
+    label = "Party Size Range",
+    inputProps = {},
+    ...wrapperProps
+  } = props;
+  const showTwenty = useTagRulesStore((store) => store.groupSizeTwenty);
+  const [minSize, setMinSize] = usePartyOption("minSize");
+  const [maxSize, setMaxSize] = usePartyOption("maxSize");
+  const [draftValue, setDraftValue] = useDraftState([minSize, maxSize]);
+
+  const marks = useMemo(
+    () =>
+      [1, 3, 6, 9, 12].concat(showTwenty ? [15, 18, 20] : []).map((n) => ({
+        value: n,
+        label: n,
+      })),
+    [showTwenty],
+  );
+
+  const onChange = useStableCallback((value) => {
+    setDraftValue(value);
+  });
+
+  const onChangeEnd = useCallback(
+    ([min, max]) => {
+      if (min !== minSize) setMinSize(min);
+      if (max !== maxSize) setMaxSize(max);
+    },
+    [minSize, maxSize, setMinSize, setMaxSize],
+  );
+
+  const description =
+    typeof minSize !== "number" || typeof maxSize !== "number"
+      ? "Use the range to set min/max party size"
+      : minSize === maxSize
+        ? `Party size of exactly ${minSize}`
+        : `Party size may range from ${minSize} to ${maxSize}`;
+
+  return (
+    <InputWrapper
+      label={label}
+      description={description}
+      pb={24}
+      {...wrapperProps}
+    >
+      <RangeSlider
+        min={1}
+        max={showTwenty ? 20 : 12}
+        minRange={0}
+        step={1}
+        value={draftValue}
+        onChange={onChange}
+        onChangeEnd={onChangeEnd}
+        marks={marks}
+        py={8}
+        {...inputProps}
+      />
+    </InputWrapper>
+  );
+};
+
 export const PartyFinderControls = () => {
   return (
     <Stack>
@@ -94,12 +143,8 @@ export const PartyFinderControls = () => {
           <MinLevelInput />
           <MaxLevelInput />
         </Group>
-        <Group wrap="nowrap">
-          <MinSizeInput />
-          <MaxSizeInput />
-        </Group>
+        <SizeRangeInput />
       </Paper>
-
       <Paper component={Stack} gap={8} p="sm" bdrs="sm">
         <ResultsGroupingSelect />
         <ActiveTagFiltersSelect />
