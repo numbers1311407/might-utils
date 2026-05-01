7c9ba97ea58cf99ac6c23a039ab6f17a97898f70
travis <numbers1311407@gmail.com>
2026-05-01 14:29:31 -0400

    fix: scrollarea in nav for small screens

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useRoute } from "wouter";
-import { Divider, Stack, NavLink, Text } from "@mantine/core";
+import { Box, ScrollArea, Divider, Stack, NavLink, Text } from "@mantine/core";
 import { useAppContext } from "@/core/context";
 import * as titles from "@/config/constants/titles";
 
@@ -42,36 +42,46 @@ const Category = ({ name, children }) => {
 
 export const Navbar = (props) => {
   return (
-    <Stack gap="lg" {...props}>
-      <NavbarLink label="Home" href="/" />
-      <Category name={titles.PARTY_CATEGORY}>
-        <NavbarLink label={titles.PARTY_FINDER_TITLE} href="/party-generator" />
-        <NavbarLink label={titles.PARTIES_TITLE} href="/parties" />
-      </Category>
-      <Category name={titles.CALCULATORS_CATEGORY}>
-        <NavbarLink
-          label={titles.MIGHT_RANGE_FINDER_TITLE}
-          href="/might-range-finder"
-        />
-        <NavbarLink label={titles.NPC_SIMULATOR_TITLE} href="/npc-simulator" />
-      </Category>
-      <Category name={titles.ROSTER_CATEGORY}>
-        <NavbarLink
-          label={titles.ROSTER_TITLE}
-          className={(active) => (active ? "active" : "")}
-          href="/roster/characters"
-        />
-        <NavbarLink
-          label={titles.ROSTER_TAGS_EDITOR_TITLE}
-          href="/roster/tags"
-        />
-        <NavbarLink label={titles.ROSTER_IO_TITLE} href="/roster/io" />
-      </Category>
-      <Category name={titles.SETTINGS_CATEGORY}>
-        <NavbarLink label={titles.TAG_RULES_TITLE} href="/rulesets" />
-        <NavbarLink label={titles.TAG_GROUPS_TITLE} href="/tag-groups" />
-        <NavbarLink label={titles.CLASS_TAGS_TITLE} href="/class-tags" />
-      </Category>
-    </Stack>
+    <Box {...props}>
+      <ScrollArea.Autosize mah="calc(100vh - 60)">
+        <Stack gap="lg" pb={70}>
+          <NavbarLink label="Home" href="/" />
+          <Category name={titles.PARTY_CATEGORY}>
+            <NavbarLink
+              label={titles.PARTY_FINDER_TITLE}
+              href="/party-generator"
+            />
+            <NavbarLink label={titles.PARTIES_TITLE} href="/parties" />
+          </Category>
+          <Category name={titles.CALCULATORS_CATEGORY}>
+            <NavbarLink
+              label={titles.MIGHT_RANGE_FINDER_TITLE}
+              href="/might-range-finder"
+            />
+            <NavbarLink
+              label={titles.NPC_SIMULATOR_TITLE}
+              href="/npc-simulator"
+            />
+          </Category>
+          <Category name={titles.ROSTER_CATEGORY}>
+            <NavbarLink
+              label={titles.ROSTER_TITLE}
+              className={(active) => (active ? "active" : "")}
+              href="/roster/characters"
+            />
+            <NavbarLink
+              label={titles.ROSTER_TAGS_EDITOR_TITLE}
+              href="/roster/tags"
+            />
+            <NavbarLink label={titles.ROSTER_IO_TITLE} href="/roster/io" />
+          </Category>
+          <Category name={titles.SETTINGS_CATEGORY}>
+            <NavbarLink label={titles.TAG_RULES_TITLE} href="/rulesets" />
+            <NavbarLink label={titles.TAG_GROUPS_TITLE} href="/tag-groups" />
+            <NavbarLink label={titles.CLASS_TAGS_TITLE} href="/class-tags" />
+          </Category>
+        </Stack>
+      </ScrollArea.Autosize>
+    </Box>
   );
 };
